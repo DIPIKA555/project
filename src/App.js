91aef0bb7fdf353b868c4f3ef58380bb322bf780
Szymon N <sniescior160@gmail.com>
2022-12-11 23:49:43 +0100

    Basic sudoku grid with number controls

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react"
+import { useNavigate } from "react-router-dom"
 import Stats from "./components/Stats"
 import useLocalStorage from "./hooks/useLocalStorage"
 
 function GameMode() {
   const [gameMode, setGameMode] = useLocalStorage('game-mode', 0)
+
+  const navigate = useNavigate()
 
   return (
     <>
@@ -14,7 +17,7 @@ function GameMode() {
         <button onClick={() => setGameMode(0)} className={gameMode === 0 ? "button button-outline-primary easy selected" : "button button-outline-primary easy"}>Easy</button>
         <button onClick={() => setGameMode(1)} className={gameMode === 1 ? "button button-outline-primary medium selected" : "button button-outline-primary medium"}>Medium</button>
         <button onClick={() => setGameMode(2)} className={gameMode === 2 ? "button button-outline-primary hard selected" : "button button-outline-primary hard"}>Hard</button>
-        <button className="button button-primary neon-button accent-font">Start game</button>
+        <button className="button button-primary neon-button accent-font" onClick={() => { navigate('/game') }}>Start game</button>
       </div>
     </>
   )
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,71 @@
 import React from 'react'
 
-function Game() {
+function ToolControl(props) {
+  <div className=''>
+
+  </div>
+}
+
+// Numbers section user can choose from
+function NumberControl(props) {
+  let buttons = Array.from({length: 9}, (_, index) => {
+    return <button className='button button-outline-primary solid' key={index + 1}>{index + 1}</button>
+  })
+
   return (
-    <div>Game</div>
+    <div className='button-wrapper flex filled'>
+      {buttons}
+    </div>
+  )
+}
+
+function Tile(props) {
+  const { number } = props
+
+  return (
+    <div className='board-tile'>
+      <h4>{number}</h4>
+    </div>
+  )
+}
+
+// Represents 3x3 square
+function TileGroup() {
+  let tiles = Array.from({length: 9}, (_, index) => {
+    return <Tile number={index} key={index} />
+  })
+
+  return (
+    <div className='tile-group'>
+      {tiles}
+    </div>
+  )
+}
+
+function Board(props) {
+  let groups = Array.from({length: 9}, (_, index) => {
+    return <TileGroup key={index} />
+  })
+
+  return (
+    <div className='board-wrapper'>
+      <div className='board-grid'>
+        {groups}
+      </div>
+    </div>
+  )
+}
+
+function Game(props) {
+  return (
+    <div className='container column'>
+      <span className='overlay'></span>
+      <header className="app-header">
+        <h1 className="headline accent-font">Sudoku</h1>
+      </header>
+      <Board />
+      <NumberControl />
+    </div>
   )
 }
 
