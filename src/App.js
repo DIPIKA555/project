bac44919f33ad5f439528f5e0ee91141a672bc44
Szymon N <sniescior160@gmail.com>
2022-12-11 22:17:06 +0100

    Home page implemented

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function GameMode() {
   return (
     <>
       <div className="helper-text">
-        <p>Select game mode</p>
+        <p>Game Mode</p>
       </div>
       <div className="button-wrapper">
         <button onClick={() => setGameMode(0)} className={gameMode === 0 ? "button button-outline-primary easy selected" : "button button-outline-primary easy"}>Easy</button>
         <button onClick={() => setGameMode(1)} className={gameMode === 1 ? "button button-outline-primary medium selected" : "button button-outline-primary medium"}>Medium</button>
         <button onClick={() => setGameMode(2)} className={gameMode === 2 ? "button button-outline-primary hard selected" : "button button-outline-primary hard"}>Hard</button>
-        <button className="button button-primary">Start game</button>
+        <button className="button button-primary neon-button accent-font">Start game</button>
       </div>
     </>
   )
@@ -25,10 +25,9 @@ function App() {
     <div className="container">
       <span className="overlay"></span>
       <header className="app-header">
-        <h1 className="headline">SUPER SUDOKU</h1>
+        <h1 className="headline accent-font">Super Sudoku</h1>
         <GameMode />
       </header>
-      <Stats />
     </div>
   )
 }
