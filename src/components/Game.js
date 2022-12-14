7b185c321e54a973be352940721acc1d438e2977
Szymon N <sniescior160@gmail.com>
2022-12-14 23:54:24 +0100

    Added sudoku class

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import Badge from './Badge'
 import ExitModal from './ExitModal'
-import { sudoku, sudokuGroups } from '../constants/sudoku'
+import { sudokuObject } from '../constants/sudoku'
+
 
 function ToolControl(props) {
   const { setOpened, hintCount, useHint } = props
@@ -41,9 +42,9 @@ function NumberControl(props) {
   const { sudokuBoard, setSudokuBoard, selected } = props
 
   const updateValue = (value) => {
-    sudoku[selected.row][selected.column] = value
-    selected.setValue(sudoku[selected.row][selected.column])
-    setSudokuBoard(sudoku)
+    sudokuObject.getSudoku()[selected.row][selected.column] = value
+    selected.setValue(sudokuObject.getSudoku()[selected.row][selected.column])
+    setSudokuBoard(sudokuObject.getSudoku())
   }
 
   let buttons = Array.from({ length: 9 }, (_, index) => {
@@ -58,9 +59,9 @@ function NumberControl(props) {
 }
 
 function Tile(props) {
-  const { setSelected, selected, row, column, group, sudokuBoard } = props
+  const { setSelected, selected, row, column, group } = props
 
-  const [value, setValue] = useState(sudoku[row][column])
+  const [value, setValue] = useState(sudokuObject.getSudoku()[row][column])
   const [current, setCurrent] = useState(false)
   const [inline, setInline] = useState(false)
 
@@ -83,7 +84,7 @@ function Tile(props) {
 }
 
 const calculateTilesValues = (group) => {
-  const { min_x, max_x, min_y, max_y } = sudokuGroups[group]
+  const { min_x, max_x, min_y, max_y } = sudokuObject.getSudokuGroups()[group]
   let array = []
   for (let i = min_y; i <= max_y; i++) {
     for (let j = min_x; j <= max_x; j++) {
@@ -133,7 +134,7 @@ function Game(props) {
   const [hintCount, setHintCount] = useState(1)   // Remaining hints count
   const [selected, setSelected] = useState({ row: -1, column: -1, group: -1, setValue: ()  => {} })
 
-  const [sudokuBoard, setSudokuBoard] = useState(sudoku)
+  const [sudokuBoard, setSudokuBoard] = useState(sudokuObject.getSudoku())
 
   const useHint = () => {
     if (hintCount > 0) { setHintCount(prev => prev - 1) }
--- src/constants/sudoku.js
+++ src/constants/sudoku.js
@@ -1,4 +1,4 @@
-const sudoku = [
+const defaultSudoku = [
     [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
     [ 5, 1, 3, 4, 5, 6, 7, 8, 9 ],
     [ 3, 0, 3, 4, 5, 6, 7, 8, 9 ],
@@ -12,7 +12,7 @@ const sudoku = [
     [ 1, 2, 3, 2, 5, 4, 6, 8, 0 ]
 ]
 
-const sudokuGroups = {
+const defaultSudokuGroups = {
     0: { min_x: 0, max_x: 2, min_y: 0, max_y: 2 },
     1: { min_x: 3, max_x: 5, min_y: 0, max_y: 2 },
     2: { min_x: 6, max_x: 8, min_y: 0, max_y: 2 },
@@ -26,7 +26,27 @@ const sudokuGroups = {
     8: { min_x: 6, max_x: 8, min_y: 6, max_y: 8 }
 }
 
-export { 
-    sudoku, 
-    sudokuGroups
+class sudokuClass {
+    constructor() {
+        this.sudoku = defaultSudoku
+        this.sudokuGroups = defaultSudokuGroups
+    }
+
+    setSudoku() {}
+
+    getSudoku() {
+        return this.sudoku
+    }
+
+    changeValue(row, column, newValue) {
+        this.sudoku[row][column] = newValue
+    }
+
+    getSudokuGroups() {
+        return this.sudokuGroups
+    }
 }
+
+const sudokuObject = new sudokuClass()
+
+export { sudokuObject }
