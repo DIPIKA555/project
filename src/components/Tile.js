d16ef97a6aabd14335cfb0db92072374d1d6d251
Szymon N <sniescior160@gmail.com>
2022-12-15 20:39:42 +0100

    Editable/non-editable fields implemented and some better fields styling

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import { sudokuObject } from '../constants/sudoku'
 
 export default function Tile(props) {
@@ -7,7 +7,8 @@ export default function Tile(props) {
     const [value, setValue] = useState(sudokuObject.getValue(row, column))
     const [current, setCurrent] = useState(false)
     const [inline, setInline] = useState(false)
-    const [same, setSame] = useState(false)     // Same value as selected field
+    const [same, setSame] = useState(false)             // Same value as selected field
+    const editable = useMemo(() => sudokuObject.checkEditableField(row, column), [row, column])
 
     useEffect(() => {
         setCurrent(selected.row === row && selected.column === column)
@@ -23,7 +24,7 @@ export default function Tile(props) {
 
     return (
         <div className={current ? 'board-tile selected' : (inline ? 'board-tile inline' : 'board-tile')} onClick={handleClick}>
-            <h4 className={same ? 'font-accent-color' : ''}>{value !== 0 ? value : ' '}</h4>
+            <h4 className={!editable && same ? 'read-only font-accent-color' : same ? 'font-accent-color' : !editable ? 'read-only' : ''}>{value !== 0 ? value : ' '}</h4>
         </div>
     )
 }
