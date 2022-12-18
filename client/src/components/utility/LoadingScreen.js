40eaab39fd566fa0e5806fc537c8739382cc1ba1
Szymon N <sniescior160@gmail.com>
2022-12-18 22:57:11 +0100

    Cancel fetching sudoku

--- client/src/components/utility/LoadingScreen.js
+++ client/src/components/utility/LoadingScreen.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function LoadingScreen(props) {
     const { loading, funFact } = props
+
+    const navigate = useNavigate()
 
     return (
         <div className={loading ? 'loader-wrapper visible' : 'loader-wrapper'}>
             <span className='overlay'></span>
             <div className='loader-content'>
+                <button className='button plain' onClick={() => navigate('/')}>
+                    <i className='bi bi-arrow-clockwise'></i>
+                </button>
                 <h3 className='accent-font'>Loading</h3>
                 <p>{funFact}</p>
                 <span className='loader'></span>
